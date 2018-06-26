a200eeadb8760f50a29f950c7f18e15a8ee679c8
stellaCmiller <stella.miller@utexas.edu>
2018-06-26 14:22:09 -0500

    thicc logic update, added enemy box and action box

--- assets/js/game2.js
+++ assets/js/game2.js
@@ -20,10 +20,13 @@ var warrior = new Fighter("warrior", 6, 3, 150);
 var knight = new Fighter("knight", 2, 4, 200);
 var mage = new Fighter("mage", 7, 2, 100);
 var ninja = new Fighter("ninja", 4, 4, 125);
+
+//Fuckloads of global variables, probably not good practice but w/e
 var preSelect = true; //Ensures that the click handler for the fighters only operates at first click
+var defenderSelect = false; //When true, allows the user to select a defender to fight
 var enemies = [];
 
-//When a fighter is selected, the rest disappear
+//When a fighter is selected, the rest move down to the defender area
 $("document").ready(
     $(".fighter").click(function () {
         while(preSelect){
@@ -54,17 +57,43 @@ $("document").ready(
     })
 )
 
-function boardSetUp(fighter) {
-    $(".fighter-box").each(function () {
-        if (!$(this).hasClass(`${fighter.name}`)) {
-            $("#enemyArea").append($(this));
+//when a defender is selected, it moves up to the current enemy box
+function chooseEnemy(){
+    $(".defender").click(function(){
+        if (defenderSelect){
+            $(this).appendTo("#currentEnemy");
+            defenderSelect = false;
+            $("#action-display").text("Press the Attack button to deal some damage!")
         }
     })
 }
 
 
+/*Closest thing I got to a main method I guess */
+function play(fighter){
+    chooseEnemy();
+    fight(fighter);
+
+}
+
+/*Puts the fighters not selected by the player into the defenders area */
+function boardSetUp(fighter) {
+    $(".fighter-box").each(function () {
+        if (!$(this).hasClass(`${fighter.name}`)) {
+            $(this).addClass("defender");
+            $("#enemyArea").append($(this));
+        }
+    })
+    $("#action-log").css("display","block");
+    defenderSelect = true;
+    play(fighter);
+}
+
+/*Resets the entire game area*/
 $("#reset").click(function(){
     $("#fighter-area").append($(".fighter-box"));
+    $(".fighter-box").removeClass("defender");
+    $("#action-log").css("display","none");
     preSelect = true;
 })
 
