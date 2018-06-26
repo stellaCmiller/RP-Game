8f2f0e7d94866a94ef0e3da0709fd2ac7df6c7bd
stellaCmiller <stella.miller@utexas.edu>
2018-06-25 22:52:55 -0500

    big logic update

--- assets/js/game2.js
+++ assets/js/game2.js
@@ -1,65 +1,76 @@
 /*Actual homework specifications */
 
 //AP = AttackPower, CAP = CounterAttackPower, HP = HealthPoints
-function Fighter(name, baseAp, cap, hp){
+function Fighter(name, baseAp, cap, hp) {
     this.name = name;
     this.baseAp = baseAp;
     this.ap = baseAp;
     this.cap = cap;
     this.hp = hp;
-    this.attack = function(){ //increases AP after each attack
-        this.ap = (this.ap + this.baseAp); 
+    this.attack = function () { //increases AP after each attack
+        this.ap = (this.ap + this.baseAp);
     }
-    this.getHP = function(){ //returns the HP of the fighter
-        return this.hp; 
+    this.getHP = function () { //returns the HP of the fighter
+        return this.hp;
     }
 }
 
 //Fighter objects are created with different stats
 var warrior = new Fighter("warrior", 6, 3, 150);
-var tank = new Fighter("tank", 2, 4, 200);
+var knight = new Fighter("knight", 2, 4, 200);
 var mage = new Fighter("mage", 7, 2, 100);
-var someFourthClass = new Fighter("dolan", 4, 4, 125);
-var fighterArray = [warrior, tank, mage, someFourthClass];
-
-//Displays the heroes' health below his picture
-fighterArray.forEach(element => {
-    let protag = $("<h3>");
-    protag.text(`Hp: ${element.getHP()}`);
-    protag.addClass("col-md-3");
-    $("#fighter-stats").append(protag);
-});
-
+var ninja = new Fighter("ninja", 4, 4, 125);
+var preSelect = true; //Ensures that the click handler for the fighters only operates at first click
+var enemies = [];
 
 //When a fighter is selected, the rest disappear
 $("document").ready(
-    $(".fighter").click(function() {
-        fighterSelector = $(this).attr("value");
-        switch(fighterSelector){
-            case "0":
-                console.log("fighter 1 was clicked");
-                play(warrior);
-                break;
+    $(".fighter").click(function () {
+        while(preSelect){
+            let fighterSelector = $(this).attr("value");
+            switch (fighterSelector) {
+                case "1":
+                    console.log("fighter 1 was clicked"); //test functions
+                    boardSetUp(warrior);
+                    break;
 
-            case "1":
-                console.log("fighter 2 was clicked");
-                play(tank);
-                break;
+                case "2":
+                    console.log("fighter 2 was clicked");
+                    boardSetUp(knight);
+                    break;
 
-            case "2":
-                console.log("fighter 3 was clicked");
-                play(mage);
-                break;
-            
-            case "3":
-                console.log("fighter 4 was clicked");
-                play(someFourthClass);
-                break;
+                case "3":
+                    console.log("fighter 3 was clicked");
+                    boardSetUp(mage);
+                    break;
+
+                case "4":
+                    console.log("fighter 4 was clicked");
+                    boardSetUp(ninja);
+                    break;
+            }
+            preSelect = false;
         }
     })
 )
 
-//Actually plays the game, woot
-function play(fighter){
-
+function boardSetUp(fighter) {
+    $(".fighter-box").each(function () {
+        if (!$(this).hasClass(`${fighter.name}`)) {
+            $("#enemyArea").append($(this));
+        }
+    })
 }
+
+
+$("#reset").click(function(){
+    $("#fighter-area").append($(".fighter-box"));
+    preSelect = true;
+})
+
+
+
+
+
+
+
